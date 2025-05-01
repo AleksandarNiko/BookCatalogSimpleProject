77cdc6bc44b1a97edeea5f42720c42a7aaba0e08
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2025-05-01 23:26:36 +0300

    Created Author controller

---
+++ BookCatalog.Web/Controllers/AuthorController.cs
@@ -0,0 +1,152 @@
+﻿using BookCatalog.Data;
+using BookCatalog.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly BookCatalogDbContext _context;
+
+        public AuthorController(BookCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Actors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Authors.ToListAsync());
+        }
+
+        // GET: Actors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Authors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // GET: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Actors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Biography,DateOfBirth")] Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(author);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        // GET: Actors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Authors.FindAsync(id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: Actors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Biography,DateOfBirth")] Author author)
+        {
+            if (id != author.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(author);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActorExists(author.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        // GET: Actors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Authors
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actor = await _context.Authors.FindAsync(id);
+            if (actor != null)
+            {
+                _context.Authors.Remove(actor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActorExists(int id)
+        {
+            return _context.Authors.Any(e => e.Id == id);
+        }
+    }
+}
